a3856cae95346c5ad80cd63a0aa0183926635993
Amirhossein Moshfeghifar <44850119+AmirMoshfeghi@users.noreply.github.com>
2019-01-14 21:14:35 +0200

    update

--- Repetition_structures/zip_boing_game.py
+++ Repetition_structures/zip_boing_game.py
@@ -1,7 +1,7 @@
 # Zip Boing program
 # Every time the number is divisible by 3 the player has to say "zip"
 # Every time the number is divisible by 7 the player has to say "boing"
-# Also, if the number is divisible by both the numbers, the player should say "zip boing".
+# Also, if divisible by both the numbers, the player should say "zip boing"
 
 # Count starts from 1
 count = 1
@@ -11,20 +11,21 @@ limit = int(input("How many numbers would you like to have? "))
 
 # Count until the limit specified by the user
 while count <= limit:
-    # Devidable by 3
-    if count % 3 == 0:
-        print("Zip")
-        count += 1
 
-    # Devidable by 7
+    # Dividable by 3 and 7
+    if count % 21 == 0:
+        print("zip boing")
+
+    # Dividable by 3
+    elif count % 3 == 0:
+        print("zip")
+
+    # Dividable by 7
     elif count % 7 == 0:
-        print("Boeing")
-        count += 1
+        print("boing")
 
-    # Devidable by 3 and 7
-    elif count % 21 == 0:
-        print("Zip Boeing")
-        count += 1
-    # Otherwise print out number and go to next
-    print(count)
+    # Otherwise print out number
+    else:
+        print(count)
+
     count += 1
